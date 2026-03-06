c40bdc7b55641f8fc16f03714d010f0e09a18658
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-07 01:49:53 +0300

    feat: add middlewares and connect to db

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 
 const PORT = process.env.PORT ?? 5000;
 const DB_URL = process.env.DB_URL ?? '';
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
 const start = async () => {
     try {
+        await mongoose.connect(DB_URL);
         app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
     } catch (e) {
         console.log(e);
