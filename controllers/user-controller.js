27bc1951a191d559c14df67745ec3ec38d22c824
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-07 21:39:43 +0300

    feat: end registration logic

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,18 @@
+const userService = require('../service/user-service')
+
 class UserController {
     async registration(req, res, next) {
         try {
-
+            const {email, password} = req.body;
+            const userData = await userService.registration(email, password);
+            res.cookies('refreshToken', userData.refreshToken,
+                {
+                    maxAge: 30 * 24 * 3600 * 1000,
+                    httpOnly: true
+                });
+            return res.json(userData);
         } catch (e) {
-
+            console.log(e);
         }
     }
     async login(req, res, next) {
