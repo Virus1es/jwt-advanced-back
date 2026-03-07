856fccfda5a2e4e07140cdce9d0c77064a1763e5
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-07 22:32:29 +0300

    fix: fix activate link

--- router/index.js
+++ router/index.js
@@ -6,7 +6,7 @@ const router = new Router();
 router.post('/registration', userController.registration);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
-router.get('/activation/:link', userController.activate);
+router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.activate);
 router.get('/users', userController.getUsers);
 
