706b32a5b6c2eb8da32e90b1a4be934380bc7ad8
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-07 22:06:54 +0300

    feat: config sending activation links logic

--- service/mail-service.js
+++ service/mail-service.js
@@ -1,6 +1,32 @@
-class MailService {
-    async sendActivationMail(to, link) {
+const nodemailer = require('nodemailer');
 
+class MailService {
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            port: process.env.MAIL_PORT,
+            secure: false,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASSWORD
+            }
+            });
+    }
+
+    async sendActivationMail(to, link) {
+        await this.transporter.sendMail({
+            from: process.env.MAIL_USER,
+            to,
+            subject: `Account activation from ${process.env.API_URL}`,
+            text: '',
+            html:
+                `
+                    <div>
+                        <h1>To activate your account go to link:</h1>
+                        <a href="${link}">${link}</a>
+                    </div>
+                `
+        })
     }
 }
 
