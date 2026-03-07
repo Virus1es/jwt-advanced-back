d8f7268e5b64cd371733733a6f77109d4be4fc4d
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-07 21:41:04 +0300

    fix: fix token-model

--- models/token-model.js
+++ models/token-model.js
@@ -1,8 +1,8 @@
 const {Schema, model} = require('mongoose');
 
-const UserSchema = new Schema({
+const TokenSchema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: 'User'},
     refreshToken: {type: String, required: true},
 });
 
-module.exports = model('User', UserSchema);
+module.exports = model('Token', TokenSchema);
